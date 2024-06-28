c62cb666c53d6f982621ad00019de37e4d9fd84a
arsnazarenko <arsnazarenko@gmail.com>
2024-06-28 15:07:13 +0300

    Add new mappings

--- config/nvim/lua/core/mappings.lua
+++ config/nvim/lua/core/mappings.lua
@@ -1,5 +1,11 @@
 vim.g.mapleader = ' '
 
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+-- Move highlighted blocks with J and K
+
+-- No search visualisation on Esc
+vim.keymap.set('n', '<esc>', ':nohlsearch<CR>', { noremap = true, silent = true })
 -- open vim explorer
 vim.keymap.set('n', '<leader>e', ":Lex<CR>")
 
