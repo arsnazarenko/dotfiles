f4cc6afc13e05ab1970fb09928ba71d0c01430d0
arsnazarenko <arsnazarenko@gmail.com>
2024-08-06 12:58:29 +0300

    Change alacritty theme

--- config/nvim/lua/core/plugin_conf/colorscheme.lua
+++ config/nvim/lua/core/plugin_conf/colorscheme.lua
@@ -1,6 +1,8 @@
-vim.cmd [[ colorscheme darcula ]]
+vim.cmd [[ colorscheme gruvbox ]]
 
 vim.cmd [[highlight Normal guibg=none ]]
 vim.cmd [[highlight NonText guibg=none ]]
 vim.cmd [[highlight Normal ctermbg=none ]]
 vim.cmd [[highlight NonText ctermbg=none ]]
+vim.cmd [[highlight SignColumn ctermbg=none ]]
+vim.cmd [[highlight SignColumn guibg=none ]]
