e61444fa8c0425c3d0ffe9eede5dec2a8dc3f9fa
arsnazarenko <arsnazarenko@gmail.com>
2024-10-10 04:57:39 +0300

    Nvim: add default remapping and telescope mappings

--- config/nvim/lua/core/mappings.lua
+++ config/nvim/lua/core/mappings.lua
@@ -1,6 +1,21 @@
+vim.keymap.set("n", "<leader>e", ":25Lex<CR>")			            -- space+e toggles netrw tree view 
+
+-- move lines
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- scrolling remappings
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- search next and prev remappings
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
 -- easy split generation
-vim.keymap.set("n", "<leader>v", ":vsplit<CR>")				        -- space+v creates a veritcal split
-vim.keymap.set("n", "<leader>s", ":split<CR>")				            -- space+s creates a horizontal split
+-- vim.keymap.set("n", "<leader>v", ":vsplit<CR>")				        -- space+v creates a veritcal split
+-- vim.keymap.set("n", "<leader>s", ":split<CR>")				            -- space+s creates a horizontal split
 
 
 -- easy split navigation
@@ -8,7 +23,6 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
--- vim.keymap.set("n", "<leader>e", ":25Lex<CR>")			            -- space+e toggles netrw tree view 
 
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
@@ -22,8 +36,6 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 -- Visual vim.keymap.sets
 vim.keymap.set("v", "<C-s>", ":sort<CR>")							-- Sort highlighted text in visual mode with Control+S
 vim.keymap.set("v", "<leader>r", "\"hy:%s/<C-r>h//g<left><left>")   -- Replace all instances of highlighted words 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")						-- Move current line down
-vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")						-- Move current line up 
 
 -- Resize splits
 vim.keymap.set("n", "<C-left>", ":vertical resize +3<CR>")
