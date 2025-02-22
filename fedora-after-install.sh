75458975a7daa191a8624e3bf9412474ab00df9c
arsnazarenko <arsnazarenko@gmail.com>
2025-02-22 15:39:53 +0300

    fedora-after-install: Add allowerasing param

--- fedora-after-install.sh
+++ fedora-after-install.sh
@@ -9,11 +9,11 @@ sudo dnf install sway firefox git # for browsing
 sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf group upgrade core
 dnf update && dnf upgrade
-sudo dnf swap 'ffmpeg-free' 'ffmpeg'
+sudo dnf swap 'ffmpeg-free' 'ffmpeg' --allowerasing
 sudo dnf install @multimedia @sound-and-video
 sudo dnf upgrade @multimedia
 sudo dnf install ffmpeg-libs libva libva-utils 
-sudo dnf swap mesa-va-drivers mesa-va-drivers-freeworld
+sudo dnf swap mesa-va-drivers mesa-va-drivers-freeworld --allowerasing
 
 sudo dnf install alsa-sof-firmware pipewire pipewire-pulseaudio wireplumber pavucontrol xdg-desktop-portal xdg-desktop-portal-wlr waybar firefox alacritty chromium tmux git neovim ripgrep wl-clipboard thunar blueman swaylock swayidle network-manager-applet NetworkManager-openvpn NetworkManager vlc obs-studio vlc-plugin-gstreamer vlc-plugin-ffmpeg keepassxc rclone cups cups-pdf dbus-daemon fontawesome-fonts-all google-noto-color-emoji-fonts cups cups-pdf gutenprint-cups system-config-printer grim slurp wlr-randr wl-mirror 
 
