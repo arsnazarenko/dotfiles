91c5a5c4163e01939f5c0ec285f078c8cef701f8
arsnazarenko <arsnazarenko@gmail.com>
2025-03-15 13:11:58 +0300

    After install script improvments

--- fedora-after-install.sh
+++ fedora-after-install.sh
@@ -6,6 +6,7 @@ max_parallel_downloads=8" > /etc/dnf/dnf.conf
 dnf update && dnf upgrade
 sudo dnf install sway firefox git # for browsing
 
+# sound and media
 sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf group upgrade core
 dnf update && dnf upgrade
@@ -15,13 +16,20 @@ sudo dnf upgrade @multimedia
 sudo dnf install ffmpeg-libs libva libva-utils 
 sudo dnf swap mesa-va-drivers mesa-va-drivers-freeworld --allowerasing
 
-sudo dnf install alsa-sof-firmware pipewire pipewire-pulseaudio wireplumber pavucontrol xdg-desktop-portal xdg-desktop-portal-wlr waybar firefox alacritty chromium tmux git neovim ripgrep wl-clipboard thunar blueman swaylock swayidle network-manager-applet NetworkManager-openvpn NetworkManager vlc obs-studio vlc-plugin-gstreamer vlc-plugin-ffmpeg keepassxc rclone cups cups-pdf dbus-daemon fontawesome-fonts-all google-noto-color-emoji-fonts cups cups-pdf gutenprint-cups system-config-printer grim slurp wlr-randr wl-mirror 
+# packages
+sudo dnf install alsa-sof-firmware pipewire pipewire-pulseaudio wireplumber pavucontrol xdg-desktop-portal xdg-desktop-portal-wlr waybar firefox alacritty chromium tmux git neovim ripgrep wl-clipboard thunar blueman swaylock swayidle network-manager-applet NetworkManager-openvpn NetworkManager vlc obs-studio vlc-plugin-gstreamer vlc-plugin-ffmpeg keepassxc rclone cups cups-pdf dbus-daemon google-noto-color-emoji-fonts cups cups-pdf gutenprint-cups system-config-printer grim slurp wlr-randr wl-mirror 
 
+# services
 systemctl --user enable mpris-proxy
 systemctl --user enable pipewire pipewire-pulse
 systemctl enable cups
 systemctl daemon-reload
 
 
-ln -sf /usr/bin/nvim /usr/local/bin/vim
+# emojis priority
 mkdir ~/.config/fontconfig && cp ./fonts.conf ~/.config/fontconfig/
+
+# gnome i3-like keybindings
+for i in {1..9}; do gsettings set "org.gnome.shell.keybindings" "switch-to-application-${i}" "[]" ; done
+for i in {1..9}; do gsettings set "org.gnome.desktop.wm.keybindings" "switch-to-workspace-$i" "['<Super>$i']" ; done
+for i in {1..9}; do gsettings set "org.gnome.desktop.wm.keybindings" "move-to-workspace-$i" "['<Shift><Super>$i']" ; done 
